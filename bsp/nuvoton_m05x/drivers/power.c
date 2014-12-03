164cd1fb120d7c87b0852e0b925cf256a4dd28da
Bright Pan <bright_pan@163.com>
2014-12-03 17:19:32 +0800

    Add power pwm function

--- bsp/nuvoton_m05x/drivers/power.c
+++ bsp/nuvoton_m05x/drivers/power.c
@@ -137,29 +137,92 @@ int rt_hw_pwr_adc_init(void)
 {
     /* Enable ADC module clock */
     CLK_EnableModuleClock(ADC_MODULE);
-
     /* ADC clock source is 50MHz, set divider to 2, ADC clock is 50/4 MHz */
     CLK_SetModuleClock(ADC_MODULE, CLK_CLKSEL1_ADC_S_HCLK, CLK_CLKDIV_ADC(4));
-
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init I/O Multi-function                                                                                 */
     /*---------------------------------------------------------------------------------------------------------*/
-
     /* Disable the P1.0 - P1.7 digital input path to avoid the leakage current */
     GPIO_DISABLE_DIGITAL_PATH(P1, 0xFF);
-
     /* Configure the P1.0 - P1.3 ADC analog input pins */
     SYS->P1_MFP &= ~(SYS_MFP_P10_Msk | SYS_MFP_P11_Msk | SYS_MFP_P12_Msk | SYS_MFP_P13_Msk |
                      SYS_MFP_P14_Msk | SYS_MFP_P15_Msk | SYS_MFP_P16_Msk | SYS_MFP_P17_Msk);
     SYS->P1_MFP |= SYS_MFP_P10_AIN0 | SYS_MFP_P11_AIN1 | SYS_MFP_P12_AIN2 | SYS_MFP_P13_AIN3 |
                    SYS_MFP_P14_AIN4 | SYS_MFP_P15_AIN5 | SYS_MFP_P16_AIN6 | SYS_MFP_P17_AIN7;
-
     return 0;
+}
 
+#define PWR_PWMA_CHANNEL_BOOST_H 0
+//#define PWR_PWM_CHANNEL_BOOST_L 1
+
+void PWMA_IRQHandler(void)
+{
+    static uint32_t cnt;
+    static uint32_t out;
+
+    // Channel 0 frequency is 100Hz, every 1 second enter this IRQ handler 100 times.
+    if (PWM_GetPeriodIntFlag(PWMA, PWR_PWMA_CHANNEL_BOOST_H))
+    {
+        if(out)
+            PWM_EnableOutput(PWMA, 0xF);
+        else
+            PWM_DisableOutput(PWMA, 0xF);
+        out ^= 1;
+        cnt = 0;
+        
+        // Clear channel 0 period interrupt flag
+        PWM_ClearPeriodIntFlag(PWMA, PWR_PWMA_CHANNEL_BOOST_H);
+    }
+}
+
+void pwr_pwm_boost_configure(uint32_t freq, uint8_t duty)
+{
+    /* Enable PWM Output pin */
+    //PWM_EnableOutput(PWMA, BIT_MASK(PWR_PWM_CHANNEL_BOOST_H) | BIT_MASK(PWR_PWM_CHANNEL_BOOST_L));
+    PWM_ConfigOutputChannel(PWMA, PWR_PWMA_CHANNEL_BOOST_H, freq, duty);
+    PWM_EnableADCTrigger(PWMA, PWR_PWMA_CHANNEL_BOOST_H, PWM_PERIOD_TRIGGER_ADC); 
+    /* Enable Timer period Interrupt */
+    //PWM_ClearPeriodIntFlag(PWMA, PWR_PWMA_CHANNEL_BOOST_H);
+    //PWM_EnablePeriodInt(PWMA, PWR_PWMA_CHANNEL_BOOST_H, PWM_PERIOD_INT_UNDERFLOW);
+    /* Enable PWMB NVIC */
+    //NVIC_EnableIRQ(PWMA_IRQn);
+}
+
+void pwr_pwm_boost_start(void)
+{
+    /* Enable PWM Timer */
+    PWM_EnableOutput(PWMA, BIT_MASK(PWR_PWMA_CHANNEL_BOOST_H));
+    PWM_Start(PWMA, BIT_MASK(PWR_PWMA_CHANNEL_BOOST_H));
+}
+void pwr_pwm_boost_stop(void)
+{
+    /* Enable PWM Timer */
+    PWM_DisableOutput(PWMA, BIT_MASK(PWR_PWMA_CHANNEL_BOOST_H));
+    PWM_ForceStop(PWMA, BIT_MASK(PWR_PWMA_CHANNEL_BOOST_H));
+}
+
+int rt_hw_pwr_pwm_init(void)
+{
+    /* Enable PWM module clock */
+    CLK_EnableModuleClock(PWM01_MODULE);
+    CLK_EnableModuleClock(PWM23_MODULE);
+    /* Select PWM module clock source */
+    CLK_SetModuleClock(PWM01_MODULE, CLK_CLKSEL1_PWM01_S_HCLK, 0);
+    CLK_SetModuleClock(PWM23_MODULE, CLK_CLKSEL1_PWM01_S_HCLK, 0);
+    /* Reset PWMA channel0~channel3 */
+    SYS_ResetModule(PWM03_RST);
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set P4 multi-function pins for PWMA Channel0 */
+    SYS->P4_MFP &= ~(SYS_MFP_P40_Msk);
+    SYS->P4_MFP |= (SYS_MFP_P40_PWM0);
+    return 0;
 }
 
 /* Initial components for device */
 INIT_DEVICE_EXPORT(rt_hw_pwr_adc_init);
+INIT_DEVICE_EXPORT(rt_hw_pwr_pwm_init);
 
 #ifdef RT_USING_FINSH
 #include <finsh.h>
@@ -168,5 +231,9 @@ FINSH_FUNCTION_EXPORT_ALIAS(pwr_adc_load_configure,pa_load, );
 FINSH_FUNCTION_EXPORT_ALIAS(pwr_adc_boost_configure, pa_boost, );
 FINSH_FUNCTION_EXPORT_ALIAS(pwr_adc_start, pa_start, );
 FINSH_FUNCTION_EXPORT_ALIAS(pwr_adc_stop, pa_stop, );
+
+FINSH_FUNCTION_EXPORT_ALIAS(pwr_pwm_boost_configure, pp_boost, );
+FINSH_FUNCTION_EXPORT_ALIAS(pwr_pwm_boost_start, pp_start, );
+FINSH_FUNCTION_EXPORT_ALIAS(pwr_pwm_boost_stop, pp_stop, );
 
 #endif
